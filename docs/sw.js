aa8e81b148c469709dc40b2d816fa3b1957f5fe8
Shoapfish <kf5660494@gmail.com>
2026-05-09 11:43:12 +0800

    appdeployment

---
+++ docs/sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'parkasense-v1'
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(['/arkasense/reservation-app.html'])
+    )
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+})
