59d8f32c415b7aa9280451506bb1bac28df873f0
Shoapfish <kf5660494@gmail.com>
2026-05-09 11:51:58 +0800

    pwaaa ; dsadasdawdsadaw

--- docs/sw.js
+++ docs/sw.js
@@ -1,15 +1,27 @@
-const CACHE = 'parkasense-v1'
+const CACHE_NAME = 'parkasense-v1'
+const ASSETS = [
+  '/arkasense/reservation-app.html',
+  '/arkasense/manifest.json'
+]
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.addAll(['/arkasense/reservation-app.html'])
-    )
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   )
+  self.skipWaiting()
 })
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(r => r || fetch(event.request))
   )
 })
